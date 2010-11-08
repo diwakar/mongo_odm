15772af0bec2d90230e66a9dc5c75f8e5c9a88ad
Diwakar <diwakar@angleritech.com>
2010-11-08 18:04:39 +0530

    'tests'

---
+++ test/lib/lib_test.rb
@@ -0,0 +1,28 @@
+
+
+  
+describe "rake sample tests" do
+
+  before(:all) do
+  end
+ 
+
+  
+  it "test2" do
+    #mock_method
+  end
+  
+  it "test3" do
+    #mock_method
+  end
+  
+  it "test4" do
+    #mock_method
+    pending("yet to do")
+
+  end
+
+  
+  
+  
+end
